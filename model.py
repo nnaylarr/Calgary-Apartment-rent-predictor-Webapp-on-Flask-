10ab2a29e07f7cd953de25523d29cf8a0a5188a9
nnaylarr <a710230070@student.ums.ac.id>
2025-06-25 08:49:52 +0000

    ubah

--- model.py
+++ model.py
@@ -6,40 +6,56 @@ Created on Tue Feb 28 19:44:47 2023
 @author: syedahmadsohail
 """
 
-# Importing the libraries
+# Importing required libraries
 import numpy as np
 import pandas as pd
 import pickle
 import sklearn
 
+# Loading the dataset
 dataset = pd.read_csv('rent.csv')
 
+# Filling missing values in 'bed_room' with 0
 dataset['bed_room'].fillna(0, inplace=True)
 
+# Filling missing values in 'area' with the mean
 dataset['area'].fillna(dataset['area'].mean(), inplace=True)
-# convert neighborhood to numeric values
+
+# Converting neighborhood names into numeric codes
 neighborhood_dict = {'NE': 4, 'NW': 5, 'SE': 6, 'SW': 7}
 dataset['location'] = dataset['location'].apply(lambda x: neighborhood_dict[x])
+
+# Selecting features
 X = dataset.iloc[:, :3]
 
-#Converting words to integer values
+# Function to convert bedroom text to integer
 def convert_to_int(word):
-    word_dict = {'one':1, 'two':2, 'three':3, 'studio':0}
+    word_dict = {'one': 1, 'two': 2, 'three': 3, 'studio': 0}
     return word_dict[word]
 
-X['bed_room'] = X['bed_room'].apply(lambda x : convert_to_int(x))
+# Applying the conversion to the 'bed_room' column
+X['bed_room'] = X['bed_room'].apply(lambda x: convert_to_int(x))
 
+# Selecting the target variable
 y = dataset.iloc[:, -1]
 
+# Training the Linear Regression model
 from sklearn.linear_model import LinearRegression
 regressor = LinearRegression()
-
-#Fitting model with trainig data
 regressor.fit(X, y)
 
-# Saving model to disk
-pickle.dump(regressor, open('model.pkl','wb'))
+# Saving the trained model to a file
+pickle.dump(regressor, open('model.pkl', 'wb'))
 
-# Loading model to compare the results
-model = pickle.load(open('model.pkl','rb'))
-print(model.predict([[2, 2200, 5]]))
+# Loading the model back from file
+model = pickle.load(open('model.pkl', 'rb'))
+
+# Sample input for prediction
+input_data = [2, 2200, 5]  # Format: [bed_room, area, location]
+
+# Simple input validation
+if not all(isinstance(i, (int, float)) for i in input_data):
+    print("Invalid input. All values must be numeric.")
+else:
+    prediction = model.predict([input_data])[0]
+    print(f"Predicted rent for input {input_data} is: ${round(prediction, 2)}")
